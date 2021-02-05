b5f94470afc983782cb043ab98b161042d1198cc
nmn009 <56015500+nmn009@users.noreply.github.com>
2021-02-05 12:32:29 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,11 +2,9 @@
 var slider = document.getElementById("volume-slider");
 var num = document.getElementById("volume-number");
 slider.oninput = function() {
-  document.getElementById("demo").innerHTML = this.value;
   num.value = this.value;
 }
 num.oninput = function (){
-document.getElementById("demo").innerHTML = this.value;
    slider.value = this.value;
 }
 // TODO
