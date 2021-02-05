81cbfac22a48a9b48720fc08a887986eb39869a7
nmn009 <56015500+nmn009@users.noreply.github.com>
2021-02-05 12:47:07 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,24 @@
 // main.js
 var slider = document.getElementById("volume-slider");
 var num = document.getElementById("volume-number");
+var soundimage = document.getElementById("volume-image");
 slider.oninput = function() {
   num.value = this.value;
+  speaker();
 }
-num.oninput = function (){
-   slider.value = this.value;
+num.oninput = function () {
+  slider.value = this.value;
+  speaker();
+}
+function speaker(){
+  if (num.value <= 67){
+    soundimage.src = "./assets/media/icons/volume-level-3.svg";
+  } else if (num.value >= 34 & num.value <=66){
+    soundimage.src = "./assets/media/icons/volume-level-2.svg";
+    else if (num.value >=1 & num.value <=33){
+      soundimage.src = "./assets/media/icons/volume-level-1.svg";
+    } else {
+      soundimage.src = "./assets/media/icons/volume-level-0.svg";
+    }
 }
 // TODO
