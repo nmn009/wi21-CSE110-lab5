d0b4bab102049f844a123165bc3cecec9d0aaad1
nmn009 <56015500+nmn009@users.noreply.github.com>
2021-02-05 13:49:52 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -10,15 +10,15 @@ num.oninput = function () {
   slider.value = this.value;
   changespeaker();
 }
-function changespeaker(){
-  if (slider.value > 66){
-    soundimage.src= "./assets/media/icons/volume-level-3.svg";
-  } else if (slider.value > 33 && slider.value < 67){
-    soundimage.src= "./assets/media/icons/volume-level-2.svg";
-    else if (slider.value == 0){
-      soundimage.src= "./assets/media/icons/volume-level-0.svg";
-    } else {
-      soundimage.src= "./assets/media/icons/volume-level-1.svg";
-    }
+function changespeaker() {
+  if (slider.value > 66) {
+    soundimage.src = "../media/icons/volume-level-3.svg";
+  } else if (slider.value < 67 && slider.value > 33){
+    soundimage.src = "../media/icons/volume-level-2.svg";
+  } else if (slider.value == 0){
+    soundimage.src = "../media/icons/volume-level-0.svg";
+  } else {
+    soundimage.src = "../media/icons/volume-level-1.svg";
+  }
 }
 // TODO
