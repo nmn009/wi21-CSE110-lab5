74a00ee2af49d04ef1085cc00a3ae64a10a34e9a
nmn009 <56015500+nmn009@users.noreply.github.com>
2021-02-05 12:17:13 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,14 +1,12 @@
 // main.js
 var slider = document.getElementById("volume-slider");
-		var num = document.getElementById("volume-number");
-		document.getElementById("demo").innerHTML = slider.value;
-		num.value = slider.value;
-		slider.oninput = function() {
-  			document.getElementById("demo").innerHTML = this.value;
-  			num.value = this.value;
-		}
-		num.oninput = function (){
-			document.getElementById("demo").innerHTML = this.value;
-    			slider.value = this.value;
-		}
+var num = document.getElementById("volume-number");
+slider.oninput = function() {
+  document.getElementById("demo").innerHTML = this.value;
+  num.value = this.value;
+}
+num.oninput = function (){
+document.getElementById("demo").innerHTML = this.value;
+   slider.value = this.value;
+}
 // TODO
