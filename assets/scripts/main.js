a3ce97a79ee8acd7ee051a5aabdc612d612c2813
nmn009 <56015500+nmn009@users.noreply.github.com>
2021-02-05 18:34:48 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,6 +2,7 @@
 var slider = document.getElementById("volume-slider");
 var num = document.getElementById("volume-number");
 var soundimage = document.getElementById("volume-image");
+var audiobtn = document.getElementById("honk-btn");
 slider.oninput = function() {
   num.value = this.value;
   changespeaker();
@@ -19,6 +20,11 @@ function changespeaker() {
     soundimage.src = "https://nmn009.github.io/wi21-CSE110-lab5/assets/media/icons/volume-level-0.svg";
   } else {
     soundimage.src = "https://nmn009.github.io/wi21-CSE110-lab5/assets/media/icons/volume-level-1.svg";
+  }
+  if (slider.value == 0){
+    audiobtn.disabled = true;
+  } else {
+    audiobtn.disabled = false;
   }
 }
 // TODO
